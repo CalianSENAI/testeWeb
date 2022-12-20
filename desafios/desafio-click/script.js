9139eceb2913a7146636b0a118dd7e03290cd7eb
Calian <calian@rapidsoft.com>
2022-12-20 16:11:11 -0300

    Resolução parcial exercício Clicadas na Tela

--- desafios/desafio-click/script.js
+++ desafios/desafio-click/script.js
@@ -7,9 +7,9 @@ document.addEventListener('click', function(event){
 
     let div = document.createElement('div')
     div.style.position = "absolute"
-    div.style.backgroundColor = "red"
-    div.style.left = "874"//event.clientX
-    div.style.top = "490"//event.clientY
+    div.style.backgroundColor = "black"
+    div.style.left = (event.clientX - 10) + "px"
+    div.style.top = (event.clientY - 10) + "px"
     div.style.borderRadius = "50%"
     div.style.height = "20px"
     div.style.width = "20px"
